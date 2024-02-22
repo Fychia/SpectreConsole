d0337a6a735e49fa546cc1130fef5ef2e3d8bd2d
WetokRafaelValerio <145479612+WetokRafaelValerio@users.noreply.github.com>
2024-02-22 18:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ SpectreConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Spectre.Console;
+
+namespace HitotsuUdemyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var table = new Table();
+
+            table.AddColumn("Quantidade de Pessoas");
+            table.AddRow(new Markup(text: "[green] 2334 [/]"));
+
+            AnsiConsole.Write(table);
+
+            Console.ReadLine();
+        }
+    }
+}
+
